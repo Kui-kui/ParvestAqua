168d12f1f96487f03ed01ff3cf3cd20981d71f45
Cédric Kui <cedrickui@gmail.com>
2017-07-02 12:16:16 +0200

    Display green header with Parvest in it

---
+++ src/components/Fundsheet/ParvestHeader.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Container, Header, Body, Title } from 'native-base';
+
+export default class ParvestHeader extends Component {
+  render() {
+    return (
+      <Container>
+        <Header
+          backgroundColor='#00965e'
+          androidStatusBarColor='#00965e'
+        >
+          <Body>
+            <Title>PARVEST AQUA</Title>
+          </Body>
+        </Header>
+      </Container>
+    );
+  }
+}
